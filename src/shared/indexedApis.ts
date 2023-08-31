a195b51dabf4b74bc356b52a75e9b3b1744adcbf
FerrariRoma <win9592@gmail.com>
2023-08-31 16:05:27 +0900

    Update : Promise 관련 수정 및 action 클래스 메소드 추가
    - resetAll, removeOne 메소드 추가
    - add 메소드에도 Promise를 반환하게 수정
    - 반복되는 초기화 에러 처리, store 호출 부분 private 메소드로 분리
    - try catch 문을 통해 DB 초기화 에러 처리

--- src/shared/indexedApis.ts
+++ src/shared/indexedApis.ts
@@ -1,6 +1,3 @@
-const DBNAME = 'extreme';
-const STORENAME = 'todos';
-
 // todo api만 셋팅해 두면 되지 않을까?
 // QUESTION : 새벽 5시 마다 리셋시키는 타이머는 어떻게 하지?.. CRON
 // QUESTION : IDB를 사용하다가 회원가입을 하면 이때까지 작성해 둔 TODO는 날라가는 것일까?..
@@ -10,17 +7,17 @@ const STORENAME = 'todos';
 addTodo * Action_add
 getOneTodo
 removeTodoOrder
-deleteTodo
+deleteTodo * Action_removeOne
 minusOrder
 updateTodo
 doTodo
 getList <- Action_getAll + Calc_orderedList
-groupByDate
+groupByDate * Calc_groupByDate
 reorderTodos
 todosToUpdate
 updateOrder
 removeTodos
-resetTodos
+resetTodos * Action_resetAll
 */
 
 interface TodoEntity {
@@ -40,6 +37,10 @@ interface AddTodoDto
   date: Date; // @Transform(({ value }) => new Date(value))
 }
 
+type transactionMode = 'readonly' | 'readwrite';
+
+const DBNAME = 'extreme';
+const STORENAME = 'todos';
 const MAX_CATEGORY_LENGTH = 5;
 
 class ETIndexedDBAction {
@@ -70,53 +71,110 @@ class ETIndexedDBAction {
     };
   }
 
-  // addTodo
-  add(todo: AddTodoDto) {
+  private getObjectStore(mode: transactionMode) {
     if (!this.db) throw new Error('DB hasnt been initialized yet');
-    if (todo.categories && todo.categories.length > MAX_CATEGORY_LENGTH) {
-      return alert('카테고리는 최대 5개 까지 설정할 수 있습니다.');
-    }
-
-    const transaction = this.db.transaction([STORENAME], 'readwrite');
+    const transaction = this.db?.transaction([STORENAME], mode);
     const todoStore = transaction.objectStore(STORENAME);
-    // TODO : 새로운 order 정보를 받아서 TODO Entity를 만들자
-    const newTodo = {
-      ...todo,
-      createdAt: new Date(),
-      done: false,
-      focusTime: 0,
-    };
-    todoStore.add(newTodo);
+    return todoStore;
+  }
+
+  // addTodo
+  add(todo: AddTodoDto): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (todo.categories && todo.categories.length > MAX_CATEGORY_LENGTH) {
+        return reject(
+          new Error('Fail to add todo', {
+            cause: '카테고리는 최대 5개 까지 설정할 수 있습니다.',
+          }),
+        );
+      }
+
+      try {
+        const todoStore = this.getObjectStore('readwrite');
+        // TODO : 새로운 order 정보를 받아서 TODO Entity를 만들자
+        const newTodo = {
+          ...todo,
+          createdAt: new Date(),
+          done: false,
+          focusTime: 0,
+        };
+        const request = todoStore.add(newTodo);
+
+        request.onsuccess = () => resolve();
+        request.onerror = (event) =>
+          reject(
+            new Error('Fail to add todo', {
+              cause: (event.target as IDBRequest).error,
+            }),
+          );
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
 
   // getList
   getAll(): Promise<TodoEntity[]> {
     // return await this.repo.find({
 
-    //   order: { date: 'ASC', order: 'ASC' },
+    //   order: { date: 'ASC', order: 'ASC' }, // -> 순서대로 정렬을 하고 나면 날짜별로도 됐을 거임
     // });
 
     return new Promise((resolve, reject) => {
-      if (!this.db) throw new Error('DB hasnt been initialized yet');
+      try {
+        const objectStore = this.getObjectStore('readonly');
+        const todoStore = objectStore.getAll();
 
-      const transaction = this.db.transaction(STORENAME, 'readonly');
-      const objectStore = transaction.objectStore(STORENAME);
-      const todoStore = objectStore.getAll();
+        todoStore.onsuccess = (event) => {
+          resolve((event.target as IDBRequest<TodoEntity[]>).result);
+        };
 
-      todoStore.onsuccess = (event) => {
-        // const fetchData = (event.target as IDBRequest<TodoEntity[]>).result;
-        // const doneTodos = this.Calc.filterDone(true, fetchData);
-        // resolve(this.Calc.orderedList(doneTodos));
-        resolve((event.target as IDBRequest<TodoEntity[]>).result);
-      };
+        todoStore.onerror = (event) => {
+          reject(
+            new Error('error fetching todo list', {
+              cause: (event.target as IDBRequest).error,
+            }),
+          );
+        };
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-      todoStore.onerror = (event) => {
-        reject(
-          new Error('error fetching todo list', {
-            cause: (event.target as IDBRequest).error,
-          }),
-        );
-      };
+  removeOne(id: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      try {
+        const objectStore = this.getObjectStore('readwrite');
+        const todoStore = objectStore.delete(id);
+        todoStore.onsuccess = () => resolve();
+        todoStore.onerror = (event) =>
+          reject(
+            new Error('Fail to delete', {
+              cause: (event.target as IDBRequest).error,
+            }),
+          );
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  resetAll(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      try {
+        const objectStore = this.getObjectStore('readwrite');
+        const todoStore = objectStore.clear();
+        todoStore.onsuccess = () => resolve();
+        todoStore.onerror = (event) =>
+          reject(
+            new Error('Fail to reset', {
+              cause: (event.target as IDBRequest).error,
+            }),
+          );
+      } catch (err) {
+        reject(err);
+      }
     });
   }
 }
