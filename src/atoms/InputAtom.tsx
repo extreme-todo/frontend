6bcc59b1b55d282edc3d93fcdae11f2f61569ad6
FerrariRoma <win9592@gmail.com>
2024-01-22 21:49:34 +0900

    Update : #46 InputAtom 스타일 커스텀 될 수 있도록 수정
    - fit-content가 적용되지 않는 버그가 있습니다. 당장에는 width들을 하드코딩했습니다.

--- src/atoms/InputAtom.tsx
+++ src/atoms/InputAtom.tsx
@@ -1,23 +1,25 @@
 import styled from '@emotion/styled';
 import { designTheme } from '../styles/theme';
+import { withTheme } from '@emotion/react';
 
 type TypePadding = `${number} ${number} ${number} ${number}`;
 type TypeLength = `${number}rem` | `${number}%` | `${number}px`;
 
 interface IInputAtomProps
   extends Pick<HTMLInputElement, 'placeholder' | 'value'> {
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   handleKeyDown?: (params: React.KeyboardEvent<HTMLInputElement>) => void;
   ariaLabel?: string;
-  // styleOption?: {
-  //   width?: TypeLength;
-  //   height?: TypeLength;
-  //   borderRadius?: TypeLength;
-  //   padding?: TypePadding;
-  //   margin?: TypePadding;
-  //   backgroundColor?: keyof typeof designTheme.colors;
-  //   outlineWidth?: TypeLength;
-  // };
+  styleOption?: {
+    width?: TypeLength;
+    height?: TypeLength;
+    borderRadius?: TypeLength;
+    padding?: TypePadding;
+    margin?: TypePadding;
+    backgroundColor?: keyof typeof designTheme.colors;
+    outlineWidth?: TypeLength;
+    fontWeight?: keyof typeof designTheme.fontSize;
+  };
 }
 
 const Usual = ({ handleChange, ariaLabel, ...props }: IInputAtomProps) => {
@@ -49,28 +51,38 @@ const InputAtom = {
 
 export default InputAtom;
 
-const UsualInput = styled.input`
-  width: 100%;
-  height: 3.898rem;
-  background-color: ${({ theme }) => theme.colors.whiteWine};
-  font-size: ${({ theme }) => theme.fontSize.h3.size};
-  font-weight: ${({ theme }) => theme.fontSize.h3.weight};
+const UsualInput = withTheme(
+  styled.input<Pick<IInputAtomProps, 'styleOption'>>(
+    ({ styleOption, theme }) => ({
+      width: styleOption?.width ?? '100%',
+      height: styleOption?.height ?? '3.898rem',
+      backgroundColor: styleOption?.backgroundColor ?? theme.colors.whiteWine,
+      fontSize: styleOption?.backgroundColor ?? theme.fontSize.h3.size,
+      fontWeight: styleOption?.fontWeight
+        ? theme.fontSize[styleOption.fontWeight].weight
+        : theme.fontSize.h3.weight,
+      borderRadius: styleOption?.borderRadius ?? '1.453rem',
+      outlineWidth: 0,
+      boxSizing: 'border-box',
+    }),
+  ),
+);
+const UnderlineInput = withTheme(
+  styled.input<Pick<IInputAtomProps, 'styleOption'>>(
+    ({ styleOption, theme }) => ({
+      width: styleOption?.width ?? '18rem', // TODO : fit-content가 안 먹는다.
+      height: styleOption?.height ?? '1.863rem',
+      backgroundColor: styleOption?.backgroundColor ?? `rgba(255, 255, 255, 0)`,
+      fontSize: styleOption?.backgroundColor ?? theme.fontSize.tag.size,
+      fontWeight: styleOption?.fontWeight
+        ? theme.fontSize[styleOption.fontWeight].weight
+        : theme.fontSize.tag.weight,
 
-  border-radius: 1.453rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-
-  outline-width: 0;
-  box-sizing: border-box;
-`;
-
-const UnderlineInput = styled.input`
-  width: 18rem;
-  height: 1.863rem;
-  border: none;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.whiteWine};
-  outline-width: 0;
-  background-color: rgba(255, 255, 255, 0);
-  font-size: ${({ theme }) => theme.fontSize.tag.size};
-  font-weight: ${({ theme }) => theme.fontSize.tag.weight};
-`;
+      outlineWidth: 0,
+      boxSizing: 'border-box',
+      border: 'none',
+      borderBottom: `1px solid ${theme.colors.whiteWine}`,
+      textAlign: 'center',
+    }),
+  ),
+);
