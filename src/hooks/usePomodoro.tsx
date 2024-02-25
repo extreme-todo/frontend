66089e0bd22389adbab55235ef365f5411145868
imhjnoh <imhjnoh@gmail.com>
2024-02-25 19:51:02 +0900

    Update : #48 집중단위, 휴식단위 배열 생성

--- src/hooks/usePomodoro.tsx
+++ src/hooks/usePomodoro.tsx
@@ -10,8 +10,10 @@ import { IChildProps } from '../shared/interfaces';
 
 export const pomodoroUnit = 60000;
 // TODO : 테스트용 1 제거 필요
-export type focusStep = 1 | 10 | 20 | 30 | 40 | 50;
-export type restStep = 1 | 5 | 10 | 15 | 20;
+export const focusStepList = [1, 10, 20, 30, 40, 50] as const;
+export const restStepList = [1, 5, 10, 15, 20] as const;
+export type focusStep = typeof focusStepList[number];
+export type restStep = typeof restStepList[number];
 export const initialPomodoroData: IPomodoroData = {
   settings: {
     focusStep: 1,
