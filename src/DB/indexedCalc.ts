673637c5f4ec3a6f4bb1fa8799205df0bd8f2ef1
FerrariRoma <win9592@gmail.com>
2024-05-30 03:15:25 +0900

    Update : #85 groupByDate 제거
    - getList에 groupByDate를 제거
    - 이로 인해서 indexedDB를 사용할 때 프론트 단에 만들어 둔 groupByDate를 사용할 곳이 아직은 마땅치 않음
    - 추후 온오프라인을 모두 관리하는 api module에서 indexedDB로 받아온 getList를 groupByDate로 후처리 하는 부분이 들어가면 좋을 것 같음

--- src/DB/indexedCalc.ts
+++ src/DB/indexedCalc.ts
@@ -9,17 +9,6 @@ class ETIndexedDBCalc {
     return todoList.sort((a, b) => (a.order as number) - (b.order as number));
   }
 
-  // QUESTION : date 타입을 string으로 해도.. 문제가 없겠지?.. 나중에 nest 서버랑 같이 쓴다고 했을 때도 문제가 없겠지?..
-  groupByDate(todoList: TodoEntity[]): Map<string, TodoEntity[]> {
-    const mapped = new Map<string, TodoEntity[]>();
-    for (const todo of todoList) {
-      const index = mapped.get(todo.date) || [];
-      index.push(todo);
-      mapped.set(todo.date, index);
-    }
-    return mapped;
-  }
-
   updateOrder(
     todoList: TodoEntity[],
     prevOrder: number,
