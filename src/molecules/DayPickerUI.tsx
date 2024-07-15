149428137dbbca980707c713357502f8a689fecf
FerrariRoma <win9592@gmail.com>
2024-07-15 22:38:40 +0900

    Update : #111 DayPickerUI에 popperAtom 적용

---
+++ src/molecules/DayPickerUI.tsx
@@ -0,0 +1,12 @@
+import { PopperAtom } from '../atoms';
+          <PopperAtom
+            popperElement={popperElement}
+            setPopperElement={setPopperElement}
+            popperRef={popperRef}
+            placement={'bottom-start'}
+            tabIndex={-1}
+            className="dialog-sheet"
+            role="dialog"
+            ariaLabel="Daypicker calendar"
+          >
+          </PopperAtom>
