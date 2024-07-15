ba5959d0c623a43d49d2a1065503d39e64212e0d
FerrariRoma <win9592@gmail.com>
2024-07-15 22:47:03 +0900

    Delete : #111 사용되지 않는 atom 및 interface 제거
    - atoms의 index.ts에 있던 GoogleLoginBtn는 실수로 popperAtom 생성 커밋에서 함께 커밋했습니다. 죄송합니다 🙇‍♂️

--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -4,12 +4,6 @@ export interface IChildProps {
   children?: ReactNode | string;
 }
 
-export interface ILayoutProps {
-  margin?: number;
-  padding?: number;
-  borderRadius?: number;
-}
-
 export interface ITotalFocusTime {
   daily: number;
   weekly: number;
