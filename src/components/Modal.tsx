36cceefae7d52ad73494c3e9d9894fc60b63db91
FerrariRoma <win9592@gmail.com>
2024-07-16 16:00:01 +0900

    Update : #111 모달 언마운트 애니메이션 추가
    - 마운트 될 때 처럼 언마운트 될 때도 슬라이드 애니메이션을 동일하게 적용하였습니다.
    - 뒷 배경색도 슬라이드 애니메이션과 같은 속도로 변하도록 하였습니다.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { IChildProps } from '../shared/interfaces';
 import { CardAtom, TypoAtom } from '../atoms';
 import IconAtom from '../atoms/IconAtom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 interface IModalProps extends IChildProps {
@@ -19,6 +19,14 @@ const queryClient = new QueryClient({
 });
 
 const Modal = ({ title, children, handleClose }: IModalProps) => {
+  const [isRender, setIsRender] = useState(true);
+
+  const closeModal = () => {
+    setIsRender(false);
+    setTimeout(() => {
+      handleClose();
+    }, 400); // 애니메이션 시간과 맞춤
+  };
   useEffect(() => {
     const preventDefault = (e: Event) => {
       e.preventDefault();
@@ -50,15 +58,15 @@ const Modal = ({ title, children, handleClose }: IModalProps) => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ModalBackground>
-        <ModalContainer>
+      <ModalBackground isRender={isRender}>
+        <ModalContainer isRender={isRender}>
           <HeaderContainer>
             <TypoAtom fontSize={'h3'} fontColor={'titleColor'}>
               {title}
             </TypoAtom>
 
             <IconAtom
-              onClick={handleClose}
+              onClick={closeModal}
               size={3.6}
               backgroundColor={'whiteWine'}
             >
@@ -75,7 +83,7 @@ const Modal = ({ title, children, handleClose }: IModalProps) => {
 export default Modal;
 export type { IModalProps };
 
-const ModalBackground = styled.div`
+const ModalBackground = styled.div<{ isRender: boolean }>`
   width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.1);
@@ -85,10 +93,34 @@ const ModalBackground = styled.div`
   position: absolute;
   top: 0;
   z-index: 99;
+
+  animation: ${({ isRender }) =>
+    isRender
+      ? 'renderAnimation 0.4s forwards'
+      : 'closeAnimation 0.4s forwards'};
+
+  @keyframes renderAnimation {
+    0% {
+      background-color: rgba(0, 0, 0, 0);
+    }
+    100% {
+      background-color: rgba(0, 0, 0, 0.1);
+    }
+  }
+
+  @keyframes closeAnimation {
+    0% {
+      background-color: rgba(0, 0, 0, 0.1);
+    }
+    100% {
+      background-color: rgba(0, 0, 0, 0);
+    }
+  }
 `;
 
-const ModalContainer = styled(CardAtom)`
+const ModalContainer = styled(CardAtom)<{ isRender: boolean }>`
   padding: 2.324375rem 3.2925rem;
+  min-width: 120px;
 
   overflow: visible;
 
@@ -101,20 +133,50 @@ const ModalContainer = styled(CardAtom)`
       rgba(255, 255, 255, 0) 55.21%
     ), ${colors.bgYellow}`};
 
+  animation: ${({ isRender }) =>
+    isRender
+      ? 'renderAnimation 0.4s forwards'
+      : 'closeAnimation 0.4s forwards'};
+
+  @keyframes renderAnimation {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  @keyframes closeAnimation {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
   /* 모바일 세로 (해상도 ~ 479px)*/
   @media all and (max-width: 479px) {
     border-radius: 30px 30px 0px 0px;
     width: 70vw;
     position: fixed;
     /* 모바일 애니메이션 */
-    animation: renderAnimation 0.4s forwards;
     @keyframes renderAnimation {
       0% {
         bottom: -100vh;
       }
       100% {
         bottom: 0;
-        position: fixed;
+      }
+    }
+
+    @keyframes closeAnimation {
+      0% {
+        bottom: 0;
+      }
+      100% {
+        bottom: -100vh;
       }
     }
   }
