df58911fb98ad5a404680cc9842595500a11aac0
FerrariRoma <win9592@gmail.com>
2024-11-05 21:38:25 +0900

    Refactor : #178 emotion의 d.ts와 theme.ts 수정
    - 경림님 작업 안에 맞춰 수정했습니다.

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -1,48 +1,55 @@
 import '@emotion/react';
 
-interface fontSize {
-  size: string;
-  weight: number;
+// 16px
+type FontRemSize = 8.75 | 2.25 | 1.875 | 1.25 | 1 | 0.875;
+// bold | semibold | regular
+type FontWeight = 700 | 600 | 400;
+interface FontAttribute {
+  size: `${FontRemSize}rem`;
+  weight: FontWeight;
 }
+type FontName = 'clock' | 'h1' | 'h2' | 'h3' | 'body' | 'b1' | 'b2';
+type FontSize = Record<FontName, FontAttribute>;
+
+type ColorAttribute = `#${string}`;
+// 8 colors tuple
+type TagColor = [
+  ColorAttribute,
+  ColorAttribute,
+  ColorAttribute,
+  ColorAttribute,
+  ColorAttribute,
+  ColorAttribute,
+  ColorAttribute,
+  ColorAttribute,
+];
+type ColorName =
+  | 'primary1'
+  | 'primary2'
+  | 'progress_bar'
+  | 'background'
+  | 'tag'
+  | 'tag_text';
+type Color = Omit<Record<ColorName, ColorAttribute>, 'tag'> & {
+  tag: TagColor;
+};
+
+type ShadowName = 'container';
+type ShadowAttribute =
+  `${number}px ${number}px ${number}px rgba(${number},${number},${number},${number})`;
+type Shadow = Record<ShadowName, ShadowAttribute>;
+
+type ResponsiveName = 'desktop' | 'tablet_h' | 'tablet_v' | 'mobile';
+type ResponsiveAttribute =
+  | `all and (min-width: ${number}px) and (max-width: ${number}px)`
+  | `all and (max-width: ${number}px)`;
+type Responsive = Record<ResponsiveName, ResponsiveAttribute>;
 
 declare module '@emotion/react' {
   export interface Theme {
-    colors: {
-      bgColor: string;
-      titleColor: string;
-      subFontColor: string;
-      accentColor: string;
-      whiteWine: string;
-      white: string;
-      lightGrey: string;
-      bgYellow: string;
-      lightGrey_2: string;
-      transparent: string;
-    };
-    shadows: {
-      basic_shadow: string;
-      button_shadow: string;
-      emboss_shadow: string;
-    };
-    fontSize: {
-      h1: fontSize;
-      h2: fontSize;
-      h3: fontSize;
-      h3_bold: fontSize;
-      h4: fontSize;
-      h5: fontSize;
-      body: fontSize;
-      body_bold: fontSize;
-      sub: fontSize;
-      tag: fontSize;
-      tooltip: fontSize;
-      switch: fontSize;
-    };
-    responsiveDevice: {
-      desktop: string;
-      tablet_h: string;
-      tablet_v: string;
-      mobile: string;
-    };
+    fontSize: FontSize;
+    color: Color;
+    shadow: Shadow;
+    responsiveDevice: Responsive;
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,37 +2,55 @@ import { Theme } from '@emotion/react';
 import { responsiveBreakpoints } from '../shared/constants';
 
 export const designTheme: Theme = {
-  colors: {
-    bgColor: '#fad390',
-    titleColor: '#4B86FA',
-    subFontColor: '#222F3E',
-    accentColor: '#ee5253',
-    whiteWine: 'rgba(108, 35, 35, 0.14)',
-    white: '#FCFCFC',
-    lightGrey: '#DFE8F1',
-    bgYellow: '#E8EAA8',
-    lightGrey_2: '#ECECEC',
-    transparent: 'rgba(0,0,0,0)',
+  color: {
+    primary1: '#523EA1',
+    primary2: '#DBFE77',
+    progress_bar: '#E44623',
+    background: '#DFDDDD',
+    tag_text: '#FFFFFF',
+    tag: [
+      '#00BD08',
+      '#C324FA',
+      '#2DF1F1',
+      '#FCAE2F',
+      '#FF7CCB',
+      '#9A98AF',
+      '#00AAD0',
+      '#CC9667',
+    ],
   },
-  shadows: {
-    basic_shadow: '0px 4px 62px rgba(0, 0, 0, 0.05)',
-    button_shadow: '0px 9px 23px rgba(0, 0, 0, 0.1)',
-    emboss_shadow:
-      'inset 0px -10px 20px rgba(41, 32, 95, 0.33), inset 0px 10px 30px rgba(255, 255, 255, 0.44)',
+  shadow: {
+    container: '0px 4px 4px rgba(0,0,0,0.25)',
   },
   fontSize: {
-    h1: { size: '6.198125rem', weight: 800 },
-    h2: { size: '3.099375rem', weight: 600 },
-    h3: { size: '2.324375rem', weight: 400 },
-    h3_bold: { size: '2.324375rem', weight: 700 },
-    h4: { size: '1.936875rem', weight: 400 },
-    h5: { size: '1.549375rem', weight: 800 },
-    body: { size: '1.743125rem', weight: 400 },
-    body_bold: { size: '1.743125rem', weight: 700 },
-    sub: { size: '1.549375rem', weight: 400 },
-    tag: { size: '1.161875rem', weight: 400 },
-    tooltip: { size: '1.25rem', weight: 300 },
-    switch: { size: '1.161875rem', weight: 700 },
+    clock: {
+      size: '8.75rem',
+      weight: 700,
+    },
+    h1: {
+      size: '2.25rem',
+      weight: 700,
+    },
+    h2: {
+      size: '1.875rem',
+      weight: 400,
+    },
+    h3: {
+      size: '1.25rem',
+      weight: 400,
+    },
+    body: {
+      size: '1rem',
+      weight: 400,
+    },
+    b1: {
+      size: '1.25rem',
+      weight: 600,
+    },
+    b2: {
+      size: '0.875rem',
+      weight: 400,
+    },
   },
   responsiveDevice: {
     desktop: `all and (min-width: ${responsiveBreakpoints.desktop.min}px) and (max-width: ${responsiveBreakpoints.desktop.max}px)`,
@@ -41,5 +59,3 @@ export const designTheme: Theme = {
     mobile: `all and (max-width: ${responsiveBreakpoints.mobile.max}px)`,
   },
 } as const;
-
-// #e55039
