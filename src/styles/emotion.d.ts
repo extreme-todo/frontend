ad1447e10ffa2a61c0e739274584116583ca06f0
FerrariRoma <win9592@gmail.com>
2024-11-11 17:38:33 +0900

    Refactor : #178 버튼 디자인 추가 및 emotion 타입 추가 리팩토링

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -1,6 +1,8 @@
 import '@emotion/react';
 
+/* font */
 // 16px
+// 140, 36, 30, 20, 16, 14
 type FontRemSize = 8.75 | 2.25 | 1.875 | 1.25 | 1 | 0.875;
 // bold | semibold | regular
 type FontWeight = 700 | 600 | 400;
@@ -11,8 +13,11 @@ interface FontAttribute {
 type FontName = 'clock' | 'h1' | 'h2' | 'h3' | 'body' | 'b1' | 'b2';
 type FontSize = Record<FontName, FontAttribute>;
 
+/* color */
 type ColorAttribute = `#${string}`;
-// 8 colors tuple
+type ColorRecord<T> = Record<T, ColorAttribute>;
+
+// 8 colors
 type TagColorName =
   | 'green'
   | 'purple'
@@ -22,16 +27,27 @@ type TagColorName =
   | 'gray'
   | 'cyan'
   | 'brown';
-type TagColor = Record<TagColorName, ColorAttribute>;
-type ColorName =
-  | 'primary1'
-  | 'primary2'
-  | 'progress_bar'
-  | 'background'
-  | 'tag'
-  | 'tag_text';
-type Color = Omit<Record<ColorName, ColorAttribute>, 'tag'> & {
+type TagColor = ColorRecord<TagColorName>;
+
+type PrimaryColorName = 'primary1' | 'primary2';
+type PrimaryColor = ColorRecord<PrimaryColorName>;
+
+type FontColorName = 'white' | 'gray';
+type FontColor = ColorRecord<FontColorName> & PrimaryColor;
+
+type BackgroundColorName = 'gray' | 'extreme_dark' | 'white' | 'extreme_orange';
+type BackgroundColor = ColorRecord<BackgroundColorName> & PrimaryColor;
+
+type ColorName = 'primary' | 'background_color' | 'tag' | 'font_color';
+
+type Color = Omit<
+  Record<ColorName, ColorAttribute>,
+  'tag' | 'primary' | 'font_color' | 'background_color'
+> & {
   tag: TagColor;
+  font_color: FontColor;
+  primary: PrimaryColor;
+  background_color: BackgroundColor;
 };
 
 type ShadowName = 'container';
@@ -45,11 +61,28 @@ type ResponsiveAttribute =
   | `all and (max-width: ${number}px)`;
 type Responsive = Record<ResponsiveName, ResponsiveAttribute>;
 
+interface ButtonColor {
+  backgroundColor: ColorAttribute | 'transparent';
+  color: ColorAttribute;
+}
+interface ButtonCommonStyle {
+  paddingHorizontal: number;
+  paddingVertical: number;
+  fontSize: FontAttribute;
+}
+type ButtonAction = 'default' | 'hover' | 'click';
+type ButtonName = 'lightBtn' | 'darkBtn' | 'textBtn';
+type Button = Record<
+  ButtonName,
+  ButtonCommonStyle & Record<ButtonAction, ButtonColor>
+>;
+
 declare module '@emotion/react' {
   export interface Theme {
     fontSize: FontSize;
     color: Color;
     shadow: Shadow;
     responsiveDevice: Responsive;
+    button: Button;
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,30 @@
 import { Theme } from '@emotion/react';
 import { responsiveBreakpoints } from '../shared/constants';
 
+const PADDING = {
+  BUTTON: {
+    NORMAL: { HORIZONTAL: 0, VERTICAL: 0 },
+    TEXT: { HORIZONTAL: 0, VERTICAL: 0 },
+  },
+};
+
 export const designTheme: Theme = {
   color: {
-    primary1: '#523EA1',
-    primary2: '#DBFE77',
-    progress_bar: '#E44623',
-    background: '#DFDDDD',
-    tag_text: '#FFFFFF',
+    primary: { primary1: '#523EA1', primary2: '#DBFE77' },
+    font_color: {
+      primary1: '#523EA1',
+      primary2: '#DBFE77',
+      white: '#FFFFFF',
+      gray: '#DFDDDD',
+    },
+    background_color: {
+      gray: '#DFDDDD',
+      primary1: '#523EA1',
+      primary2: '#DBFE77',
+      white: '#FFFFFF',
+      extreme_dark: '#1C1C1D',
+      extreme_orange: '#E44623',
+    },
     tag: {
       green: '#00BD08',
       purple: '#C324FA',
@@ -57,5 +74,64 @@ export const designTheme: Theme = {
     tablet_h: `all and (min-width: ${responsiveBreakpoints.tablet_h.min}px) and (max-width: ${responsiveBreakpoints.tablet_h.max}px)`,
     tablet_v: `all and (min-width: ${responsiveBreakpoints.tablet_v.min}px) and (max-width: ${responsiveBreakpoints.tablet_v.max}px)`,
     mobile: `all and (max-width: ${responsiveBreakpoints.mobile.max}px)`,
+  },
+  button: {
+    lightBtn: {
+      paddingHorizontal: PADDING.BUTTON.NORMAL.HORIZONTAL,
+      paddingVertical: PADDING.BUTTON.NORMAL.VERTICAL,
+      fontSize: {
+        size: '1.25rem',
+        weight: 600,
+      },
+      default: {
+        backgroundColor: '#523EA1CC',
+        color: '#523EA1',
+      },
+      hover: {
+        backgroundColor: '#523EA1B3',
+        color: '#523EA1',
+      },
+      click: {
+        backgroundColor: '#523EA1',
+        color: '#DBFE77',
+      },
+    },
+    darkBtn: {
+      paddingHorizontal: PADDING.BUTTON.NORMAL.HORIZONTAL,
+      paddingVertical: PADDING.BUTTON.NORMAL.VERTICAL,
+      fontSize: {
+        size: '1.25rem',
+        weight: 600,
+      },
+      default: {
+        backgroundColor: '#DBFE77CC',
+        color: '#DBFE77',
+      },
+      hover: {
+        backgroundColor: '#DBFE77B3',
+        color: '#DBFE77',
+      },
+      click: {
+        backgroundColor: '#DBFE77',
+        color: '#523EA1',
+      },
+    },
+    textBtn: {
+      paddingHorizontal: PADDING.BUTTON.TEXT.HORIZONTAL,
+      paddingVertical: PADDING.BUTTON.TEXT.VERTICAL,
+      fontSize: { size: '0.875rem', weight: 400 },
+      default: {
+        backgroundColor: 'transparent',
+        color: '#523EA1',
+      },
+      hover: {
+        backgroundColor: '#DFDDDD',
+        color: '#523EA1',
+      },
+      click: {
+        backgroundColor: '#523EA1',
+        color: '#DFDDDD',
+      },
+    },
   },
 } as const;
