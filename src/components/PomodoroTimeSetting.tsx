6bc2f32aaceba87fcedf0fc27eefa639f908d357
FerrariRoma <win9592@gmail.com>
2025-03-14 02:26:24 +0900

    Refactor : #203 TimeSetter Refactor

--- src/components/PomodoroTimeSetting.tsx
+++ src/components/PomodoroTimeSetting.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { BtnAtom, IconAtom, TypoAtom } from '../atoms';
+import { BtnAtom, CardAtom, IconAtom, TypoAtom } from '../atoms';
 import { FlipCounter } from '../molecules';
 import {
   focusStepList,
@@ -11,65 +11,58 @@ import {
 import styled from '@emotion/styled';
 
 interface ITimeCounterProps {
-  flipData: Array<number>;
-  title: string;
-  flipIndex: number;
-  isPlus: boolean;
-  handlePlus: () => void;
-  handleMinus: () => void;
+  timeTitle: string;
+  time: number;
+  handleTimeUp: () => void;
+  handleTimeDown: () => void;
 }
 
 const TimeSetter = ({
-  flipData,
-  title,
-  flipIndex,
-  isPlus,
-  handlePlus,
-  handleMinus,
+  timeTitle,
+  time,
+  handleTimeUp,
+  handleTimeDown,
 }: ITimeCounterProps) => {
   return (
-    <TimeSetterContainer>
-      <TypoAtom fontSize={'body'} className="timeSetterTypo">
-        {title}
-      </TypoAtom>
-      <FlipHandlerContainer>
-        <BtnAtom handleOnClick={handlePlus}>
-          <IconAtom
-            size={1}
-            className="timeHandler"
-            alt="timeup"
-            src={'icons/btnUp.svg'}
-          />
-        </BtnAtom>
-        <FlipContainer>
-          <FlipCounter>
-            {flipData.map((data) => (
-              <FlipCounter.Flipper
-                key={data}
-                className={
-                  (data === flipData[flipIndex - 1] ? 'prev' : '') +
-                  (data === flipData[flipIndex] ? 'curr ' : '') +
-                  (data === flipData[flipIndex + 1] ? 'next ' : '')
-                }
-                isPlus={isPlus}
-                flipNumber={data}
-              />
-            ))}
-          </FlipCounter>
-          <TypoAtom fontSize="body" className="timeSetterTypo">
-            분
+    <div style={{ outline: '1px solid red' }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Rectangle />
+        <TypoAtom fontSize="h2" fontColor="primary2">
+          {timeTitle}
+        </TypoAtom>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <ClockTypoContainer>
+          <ClockTypo fontColor="primary2">{time}</ClockTypo>
+        </ClockTypoContainer>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+            marginLeft: '0.5625rem',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              marginBottom: '0.625rem',
+            }}
+          >
+            <BtnAtom handleOnClick={handleTimeUp}>
+              <IconAtom src={'icon/pomodoroTimeUp.svg'} />
+            </BtnAtom>
+            <BtnAtom handleOnClick={handleTimeDown}>
+              <IconAtom src={'icon/pomodoroTimeDown.svg'} />
+            </BtnAtom>
+          </div>
+          <TypoAtom fontColor="primary2" fontSize="b1">
+            Minute
           </TypoAtom>
-        </FlipContainer>
-        <BtnAtom handleOnClick={handleMinus}>
-          <IconAtom
-            size={1}
-            className="timeHandler"
-            alt="timedown"
-            src={'icons/btnDown.svg'}
-          />
-        </BtnAtom>
-      </FlipHandlerContainer>
-    </TimeSetterContainer>
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -123,136 +116,30 @@ const PomodoroTimeSetting = () => {
     if (newTime.resetFocus !== restStep) setRestStep(newTime.resetFocus);
   };
 
-  return (
-    <>
-      <PomodoroTimeSettingContainer>
-        <TimeSetter
-          flipData={[...focusStepList]}
-          title={'집중시간'}
-          flipIndex={focusFlipIndex}
-          isPlus={isPlus}
-          handlePlus={handleFocusPlus}
-          handleMinus={handleFocusMinus}
-        />
-        <TimeSetter
-          flipData={[...restStepList]}
-          title={'휴식시간'}
-          flipIndex={restFlipIndex}
-          isPlus={isPlus}
-          handlePlus={handleRestPlus}
-          handleMinus={handleRestMinus}
-        />
-      </PomodoroTimeSettingContainer>
-      <FooterContainer>
-        <BtnAtom handleOnClick={handleSubmit}>
-          <IconAtom
-            size={4.455}
-            backgroundColor="primary1"
-            alt="timesubmit"
-            src={'icons/ok.svg'}
-          />
-        </BtnAtom>
-      </FooterContainer>
-    </>
-  );
+  return <CardAtom bg="primary1"></CardAtom>;
 };
 
 export default PomodoroTimeSetting;
 export { TimeSetter };
 
-const TimeSetterContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  @media ${({ theme }) => theme.responsiveDevice.tablet_v},
-    ${({ theme }) => theme.responsiveDevice.mobile} {
-    .timeSetterTypo {
-      font-size: ${({ theme }) => theme.fontSize.h3.size};
-    }
-  }
+const Rectangle = styled.div`
+  width: 1.25rem;
+  height: 1.25rem;
+  background-color: ${({
+    theme: {
+      color: { backgroundColor },
+    },
+  }) => backgroundColor.primary2};
+  margin-right: 0.375rem;
 `;
 
-const FlipHandlerContainer = styled.div`
-  width: 16.3125rem;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  border-radius: 1.4525rem;
-  margin-top: 0.6875rem;
-  padding: 0.6875rem 0.5625rem;
-  background-color: rgb(219, 209, 164);
-
-  .timeHandler {
-    padding: 1rem 0;
-    width: 100%;
-    height: 100%;
-  }
-  @media ${({ theme }) => theme.responsiveDevice.tablet_v},
-    ${({ theme }) => theme.responsiveDevice.mobile} {
-    width: 20rem;
-    img {
-      width: 3rem;
-      height: 3rem;
-    }
-  }
+const ClockTypo = styled(TypoAtom)`
+  font-weight: 700;
+  font-size: 8.75rem;
+  line-height: 6.25rem;
 `;
 
-const FlipContainer = styled.div`
-  width: 100%;
-
-  padding-top: 0.625rem;
-  padding-bottom: 0.625rem;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  > :nth-of-type(2) {
-    margin-left: 1.5625rem;
-  }
-
-  @media ${({ theme }) => theme.responsiveDevice.tablet_v},
-    ${({ theme }) => theme.responsiveDevice.mobile} {
-    height: 100%;
-  }
-`;
-
-const PomodoroTimeSettingContainer = styled.div`
-  display: flex;
-
-  & > div:first-of-type {
-    margin-right: 5.4375rem;
-  }
-
-  @media ${({ theme }) => theme.responsiveDevice.tablet_v},
-    ${({ theme }) => theme.responsiveDevice.mobile} {
-    flex-direction: column;
-    align-items: center;
-    row-gap: 4rem;
-    & > div:first-of-type {
-      margin-right: 0;
-    }
-  }
-`;
-
-const FooterContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-  margin-top: 0.75rem;
-  @media ${({ theme }) => theme.responsiveDevice.tablet_v},
-    ${({ theme }) => theme.responsiveDevice.mobile} {
-    div {
-      width: 4.5rem;
-      height: 4.5rem;
-      border-radius: 50%;
-      margin-top: 3rem;
-    }
-    img {
-      width: 3rem;
-      height: 3rem;
-    }
-  }
+const ClockTypoContainer = styled.div`
+  padding: 1.5rem 0.71875rem 1.25rem 0.40625rem;
+  border-bottom: 2px solid #dbfe77;
 `;
