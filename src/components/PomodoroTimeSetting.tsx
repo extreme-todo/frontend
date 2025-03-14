f27cab9b6a46b1e45d20502ae20776084a81fdad
FerrariRoma <win9592@gmail.com>
2025-03-14 20:59:59 +0900

    Refactor : #203 Pomodoro Time Setting UI 작업 완료

--- src/components/PomodoroTimeSetting.tsx
+++ src/components/PomodoroTimeSetting.tsx
@@ -26,7 +26,6 @@ const TimeSetter = ({
   return (
     <div
       style={{
-        outline: '1px solid red',
         width: '17.5625rem',
       }}
     >
@@ -53,13 +52,42 @@ const TimeSetter = ({
               display: 'flex',
               flexDirection: 'column',
               marginBottom: '0.625rem',
+              margin: 'auto',
             }}
           >
-            <BtnAtom handleOnClick={handleTimeUp}>
-              <IconAtom src={'icon/pomodoroTimeUp.svg'} />
+            <BtnAtom handleOnClick={handleTimeUp} btnType="lightBtn">
+              <svg
+                width="2.5rem"
+                height="2.5rem"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="svgBtn"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M20.0001 14.3586L9.9723 24.3864L7.29297 21.707L20.0001 8.99992L32.7072 21.707L30.0279 24.3864L20.0001 14.3586Z"
+                  fill="#DBFE77"
+                />
+              </svg>
             </BtnAtom>
             <BtnAtom handleOnClick={handleTimeDown}>
-              <IconAtom src={'icon/pomodoroTimeDown.svg'} />
+              <svg
+                width="2.5rem"
+                height="2.5rem"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="svgBtn"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M19.9999 25.0555L30.0277 15.0277L32.707 17.707L19.9999 30.4141L7.29282 17.707L9.97215 15.0277L19.9999 25.0555Z"
+                  fill="#DBFE77"
+                />
+              </svg>
             </BtnAtom>
           </div>
           <TypoAtom fontColor="primary2" fontSize="b1">
@@ -117,32 +145,101 @@ const PomodoroTimeSetting = () => {
   };
 
   return (
-    <CardAtom
-      bg="primary1"
-      style={{ display: 'flex', flexDirection: 'row', columnGap: '3rem' }}
-    >
-      <TimeSetter
-        timeTitle={'집중시간'}
-        time={focusStepList[focusTimeIndex]}
-        handleTimeUp={handleFocusUp}
-        handleTimeDown={handleFocusDown}
-      />
-      <TimeSetter
-        timeTitle={'휴식시간'}
-        time={restStepList[restTimeIndex]}
-        handleTimeUp={handleRestUp}
-        handleTimeDown={handleRestDown}
-      />
-      <BtnAtom handleOnClick={handleSubmit}>저장</BtnAtom>
-      <BtnAtom handleOnClick={() => alert('닫기')}>
-        <IconAtom src={'icon/closeYellow.svg'} size={2} />
-      </BtnAtom>
-    </CardAtom>
+    <PomodoroCardAtom bg="primary1">
+      <div>
+        <TimeSetter
+          timeTitle={'집중시간'}
+          time={focusStepList[focusTimeIndex]}
+          handleTimeUp={handleFocusUp}
+          handleTimeDown={handleFocusDown}
+        />
+        <TimeSetter
+          timeTitle={'휴식시간'}
+          time={restStepList[restTimeIndex]}
+          handleTimeUp={handleRestUp}
+          handleTimeDown={handleRestDown}
+        />
+      </div>
+
+      <div>
+        <BtnAtom handleOnClick={handleSubmit} btnType="extremeDarkBtn">
+          <div style={{ paddingLeft: '2rem', paddingRight: '2rem' }}>
+            <TypoAtom fontColor="primary2" fontSize="b1">
+              저장
+            </TypoAtom>
+          </div>
+        </BtnAtom>
+        <BtnAtom handleOnClick={() => alert('닫기')}>
+          <svg
+            width="2rem"
+            height="2rem"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="svgBtn"
+          >
+            <rect
+              width="1.81331"
+              height="25.3864"
+              transform="matrix(-0.707107 0.707107 0.707107 0.707107 7.28223 6.19324)"
+              fill="#DBFE77"
+            />
+            <rect
+              x="7.28223"
+              y="25.233"
+              width="1.81331"
+              height="25.3864"
+              transform="rotate(-135 7.28223 25.233)"
+              fill="#DBFE77"
+            />
+          </svg>
+        </BtnAtom>
+      </div>
+    </PomodoroCardAtom>
   );
 };
 
 export default PomodoroTimeSetting;
 export { TimeSetter };
+
+const PomodoroCardAtom = styled(CardAtom)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  .svgBtn path,
+  .svgBtn rect {
+    opacity: 1;
+    transition: opacity 0.3s ease-in-out;
+  }
+  .svgBtn:hover path,
+  .svgBtn:hover rect {
+    opacity: 0.4;
+  }
+
+  & > div:first-of-type {
+    display: flex;
+
+    & > div:first-of-type {
+      margin-right: 2rem;
+    }
+  }
+
+  & > div:last-of-type {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: flex-end;
+
+    & > button:first-of-type {
+      margin: auto;
+    }
+
+    & > button:last-of-type {
+      align-self: flex-start;
+    }
+  }
+`;
 
 const Rectangle = styled.div`
   width: 1.25rem;
@@ -162,8 +259,9 @@ const ClockTypo = styled(TypoAtom)`
 `;
 
 const ClockTypoContainer = styled.div`
-  padding: 1.5rem 0.71875rem 1.25rem 0.40625rem;
-  width: 12.6875rem;
+  padding-top: 1.5rem;
+  padding-bottom: 1.25rem;
+  width: 11.5rem;
   border-bottom: 2px solid #dbfe77;
   text-align: center;
 `;
