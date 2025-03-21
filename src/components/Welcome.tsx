66556c32a7e2faced9416453e543edc907142860
FerrariRoma <win9592@gmail.com>
2025-03-21 20:48:56 +0900

    Refactor : #206 Welcome 스크린 리팩토링 완료

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,17 +1,17 @@
 import { useContext, useRef, useState } from 'react';
-import { createPortal } from 'react-dom';
 
 import { BtnAtom, IconAtom, TypoAtom } from '../atoms';
-import Modal from './Modal';
-import Setting from './Setting';
 
-import { usersApi } from '../shared/apis';
+import { rankingApi, todosApi, usersApi } from '../shared/apis';
 import { LoginContext } from '../hooks';
 
 import styled from '@emotion/styled';
+import { MainLogo } from '../svg/MainLogo';
+import { useInView, useAnimation } from 'framer-motion';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const Welcome = () => {
-  const [isModal, setIsModal] = useState<boolean>(false);
+  const [isSettingModal, setIsSettingModal] = useState<boolean>(false);
   const { isLogin, deleteToken } = useContext(LoginContext);
   const welcomeRef = useRef<HTMLDivElement>(null);
 
@@ -24,32 +24,99 @@ const Welcome = () => {
   };
 
   const handleSetting = (): void => {
-    setIsModal((prev) => !prev);
+    setIsSettingModal(true);
   };
 
   const handleClose = (): void => {
-    setIsModal(false);
+    setIsSettingModal(false);
   };
+
+  const handleReset = async () => {
+    if (!window.confirm('정말로 기록을 초기화 하시겠습니까?')) return;
+    await Promise.all([todosApi.resetTodos(), rankingApi.resetRanking()]);
+  };
+
+  const handleWithdrawal = async () => {
+    if (!window.confirm('정말로 회원 탈퇴하시겠습니까?')) return;
+    console.debug('handleWithdrawal 작동');
+    await usersApi.withdrawal();
+  };
+
+  const queryClient = useQueryClient();
+  const { mutate: resetMutation } = useMutation(handleReset, {
+    onSuccess() {
+      window.alert('초기화 성공');
+      queryClient.invalidateQueries(['todos']);
+    },
+    onError(error) {
+      console.error(
+        '\n\n\n 🚨 error in SettingModal‘s useMutation 🚨 \n\n',
+        error,
+      );
+    },
+  });
+  const { mutate: withdrawMutation } = useMutation(handleWithdrawal, {
+    onSuccess() {
+      window.alert('회원 탈퇴 성공');
+      queryClient.invalidateQueries(['todos']);
+      queryClient.invalidateQueries(['category']);
+      handleClose();
+      deleteToken();
+    },
+    onError(error) {
+      console.error(
+        '\n\n\n 🚨 error in SettingModal‘s useMutation 🚨 \n\n',
+        error,
+      );
+    },
+  });
 
   return (
     <WelcomeContainer ref={welcomeRef}>
-      <img className="logo" src="/icon/logo.svg" alt="logo" />
+      <MainLogo />
       {isLogin ? (
-        <BtnContainer>
-          <BtnAtom handleOnClick={handleLogoutBtn}>
-            <TypoAtom fontSize="b1">SIGN OUT</TypoAtom>
-          </BtnAtom>
-          <BtnAtom handleOnClick={handleSetting}>
-            <TypoAtom fontSize="b1">SETTING</TypoAtom>
-          </BtnAtom>
-          {isModal &&
-            createPortal(
-              <Modal title="설정" handleClose={handleClose}>
-                <Setting handleClose={handleClose} />
-              </Modal>,
-              welcomeRef.current as HTMLDivElement,
-            )}
-        </BtnContainer>
+        isSettingModal ? (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              rowGap: '1.25rem',
+              alignItems: 'center',
+            }}
+          >
+            <BtnAtom handleOnClick={resetMutation} ariaLabel="reset">
+              <TypoAtom fontSize="body" fontColor="extreme_orange">
+                데이터 초기화
+              </TypoAtom>
+            </BtnAtom>
+            <BtnAtom handleOnClick={withdrawMutation} ariaLabel="withdraw">
+              <TypoAtom fontSize="body" fontColor="extreme_orange">
+                회원 탈퇴
+              </TypoAtom>
+            </BtnAtom>
+            <BtnAtom handleOnClick={handleClose} ariaLabel="goback">
+              <IconAtom size={2} src="/icon/closeOrange.svg" />
+            </BtnAtom>
+          </div>
+        ) : (
+          <LoginContainer>
+            <BtnAtom handleOnClick={handleSetting} ariaLabel="setting">
+              <IconAtom size={1.25} src="/icon/setting.svg" />
+              <TypoAtom fontColor="extreme_orange" fontSize="b2">
+                설정
+              </TypoAtom>
+            </BtnAtom>
+            <TypoAtom fontColor="extreme_orange" fontSize="b2">
+              |
+            </TypoAtom>
+            <BtnAtom handleOnClick={handleLogoutBtn} ariaLabel="logout">
+              <IconAtom size={1.25} src="/icon/logout.svg" />
+              <TypoAtom fontColor="extreme_orange" fontSize="b2">
+                로그아웃
+              </TypoAtom>
+            </BtnAtom>
+          </LoginContainer>
+        )
       ) : (
         <BtnAtom handleOnClick={handleLoginBtn} className="login_button">
           <TypoAtom fontColor="extreme_orange" fontSize="b1">
@@ -75,11 +142,11 @@ const WelcomeContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  row-gap: 1.5rem;
 
-  .logo {
+  #logo {
     width: 33.75rem;
     height: 11.25rem;
+    margin-bottom: 3.0625rem;
   }
 
   .login_button {
@@ -89,6 +156,7 @@ const WelcomeContainer = styled.div`
       transition: opacity 0.3s ease-in-out;
     }
   }
+
   .google_logo {
     width: 6.25rem;
     height: 1.75rem;
@@ -103,21 +171,16 @@ const WelcomeContainer = styled.div`
   }
 `;
 
-const BtnContainer = styled.div`
+const LoginContainer = styled.div`
   display: flex;
-  justify-content: space-around;
-  width: 23.375rem;
-  height: 2.375rem;
+  align-items: center;
+  column-gap: 1.25rem;
 
-  > div {
-    line-height: 30px;
-    color: ${({ theme: { color } }) => color.fontColor.primary1};
-    margin: auto;
-  }
-  > div:nth-of-type(2) {
-    width: 4px;
-    height: 31px;
-    background: rgba(108, 35, 35, 0.14);
-    transform: rotate(45deg);
+  > button {
+    display: flex;
+    align-items: center;
+    > img {
+      margin-right: 0.25rem;
+    }
   }
 `;
--- src/test/components/Welcome.test.tsx
+++ src/test/components/Welcome.test.tsx
@@ -1,19 +1,13 @@
-import React, { ReactNode } from 'react';
+import React, { act } from 'react';
 import { Welcome } from '../../components/index';
 
 import { ThemeProvider } from '@emotion/react';
 import { designTheme } from '../../styles/theme';
-import { usersApi } from '../../shared/apis';
+import { rankingApi, todosApi, usersApi } from '../../shared/apis';
 
 import { mockLocalStorage } from '../../../fixture/mockLocalStorage';
 
-import {
-  act,
-  fireEvent,
-  render,
-  screen,
-  logRoles,
-} from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { LoginProvider } from '../../hooks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -53,11 +47,9 @@ describe('Welcome', () => {
     });
 
     it('로고가 있다.', () => {
-      const { getByRole } = renderResult;
-
-      expect(getByRole('img', { name: /logo/i })).toBeInTheDocument();
+      const { container } = renderResult;
+      expect(container.querySelector('#logo')).toBeInTheDocument();
     });
-
     describe('유저가 로그인을 하지 않은 경우', () => {
       it('구글 소셜 로그인 버튼이 있다.', () => {
         const { getByText, getByRole } = renderResult;
@@ -67,63 +59,93 @@ describe('Welcome', () => {
         ).toBeInTheDocument();
       });
       it('구글 소셜 버튼을 누르면 로그인 api가 호출된다.', () => {
-        const { getByText, getByRole } = renderResult;
+        const { getByRole } = renderResult;
         const spyOnLogin = jest.spyOn(usersApi, 'login').mockImplementation();
         const googleImage = getByRole('img', { name: /google_login_button/i });
         fireEvent.click(googleImage);
         expect(spyOnLogin).toBeCalled();
       });
-      // describe('유저가 로그인을 했을 경우', () => {
-      //   let renderResult: HTMLElement;
-      //   const mockDeleteLocalStorage = jest.fn();
-      //   beforeEach(() => {
-      //     renderResult = mockWelcome(
-      //       jest.fn((key: string) => 'extremeTokemSample'),
-      //       jest.fn(),
-      //       mockDeleteLocalStorage,
-      //     );
-      //   });
-      //   it('로그아웃 버튼이 렌더링 되어야 하고,', () => {
-      //     expect(renderResult).toContainElement(screen.getByText('SIGN OUT'));
-      //   });
-      //   it('클릭하면 removeItem을 호출한다.', () => {
-      //     const logoutBtn = screen.getByText('SIGN OUT');
-      //     fireEvent.click(logoutBtn);
-      //     expect(mockDeleteLocalStorage).toBeCalled();
-      //   });
-      //   it('셋팅 버튼이 렌더링 되어야 하고,', () => {
-      //     expect(renderResult).toContainElement(screen.getByText('SETTING'));
-      //   });
-      //   it('클릭하면 셋팅 모달을 띄워준다.', () => {
-      //     const settingBtn = screen.getByText('SETTING');
-      //     fireEvent.click(settingBtn);
-      //     act(() => {
-      //       const settingTitle = screen.getByText('설정');
-      //       expect(settingTitle).toBeInTheDocument();
-      //     });
-      //   });
     });
 
     describe('유저가 로그인을 한 경우', () => {
-      it('로그아웃 버튼이 렌더링 되어야 하고', () => {});
-      it('설정 버튼이 렌더링 되어야 한다.', () => {});
+      const mockDeleteLocalstorage = jest.fn();
+      beforeEach(() => {
+        renderResult = mockWelcome(
+          jest.fn((key: string) => 'token_something'),
+          jest.fn(),
+          mockDeleteLocalstorage,
+        );
+      });
+      it('로그아웃 버튼이 렌더링 되어야 하고', () => {
+        const { getByRole } = renderResult;
+        expect(getByRole('button', { name: /logout/i })).toBeInTheDocument();
+      });
+      it('설정 버튼이 렌더링 되어야 한다.', () => {
+        const { getByRole } = renderResult;
+        expect(getByRole('button', { name: /setting/i })).toBeInTheDocument();
+      });
+      it('로그아웃 버튼을 누르면 localStorage의 토큰과 사용자 정보가 제거된다.', () => {
+        const { getByRole } = renderResult;
+        const logout = getByRole('button', { name: /logout/i });
+        fireEvent.click(logout);
+        expect(mockDeleteLocalstorage).toBeCalledTimes(2);
+      });
     });
+
     describe('설정 버튼을 누르면', () => {
-      it('데이터 초기화 버튼이 렌더링 되어야 하고', () => {});
-      it('회원 탈퇴 버튼이 렌더링 되어야 한다.', () => {});
-      it('데이터 초기화 버튼을 누르면 removeItem을 호출한다.', () => {});
-      it('회원 탈퇴 버튼을 누르면 withdrawal을 호출한다.', () => {});
+      const mockDeleteLocalstorage = jest.fn();
+      beforeEach(() => {
+        renderResult = mockWelcome(
+          jest.fn((key: string) => 'token_something'),
+          jest.fn(),
+          mockDeleteLocalstorage,
+        );
+        const setting = renderResult.getByRole('button', { name: /setting/ });
+        fireEvent.click(setting);
+      });
+
+      it('데이터 초기화 버튼이 렌더링 되어야 하고', () => {
+        const { getByRole } = renderResult;
+        expect(getByRole('button', { name: /reset/i })).toBeInTheDocument();
+      });
+      it('회원 탈퇴 버튼이 렌더링 되어야 한다.', () => {
+        const { getByRole } = renderResult;
+        expect(getByRole('button', { name: /withdraw/i })).toBeInTheDocument();
+      });
+      it('데이터 초기화 버튼을 누르면 removeItem을 호출한다.', async () => {
+        const { getByRole } = renderResult;
+        jest.spyOn(window, 'alert').mockImplementation();
+        jest.spyOn(window, 'confirm').mockImplementation(() => true);
+        const reset = getByRole('button', { name: /reset/i });
+        const mockResetTodos = jest
+          .spyOn(todosApi, 'resetTodos')
+          .mockImplementation();
+        const mockResetRanking = jest
+          .spyOn(rankingApi, 'resetRanking')
+          .mockImplementation();
+        await act(() => fireEvent.click(reset));
+        expect(mockResetTodos).toBeCalled();
+        expect(mockResetRanking).toBeCalled();
+      });
+      it('회원 탈퇴 버튼을 누르면 withdrawal을 호출한다.', async () => {
+        const mockConfirm = jest
+          .spyOn(usersApi, 'withdrawal')
+          .mockImplementation(() => Promise.resolve());
+        jest.spyOn(window, 'alert').mockImplementation();
+        jest.spyOn(window, 'confirm').mockImplementation(() => true);
+        const { getByRole } = renderResult;
+        const withdraw = getByRole('button', { name: /withdraw/i });
+        await act(() => fireEvent.click(withdraw));
+        expect(mockConfirm).toBeCalled();
+      });
+      it('X 아이콘을 누르면 뒤로 간다.', () => {
+        const { getByRole } = renderResult;
+        const xButton = getByRole('button', { name: 'goback' });
+        fireEvent.click(xButton);
+        const setting = getByRole('button', { name: 'setting' });
+        expect(xButton).not.toBeInTheDocument();
+        expect(setting).toBeInTheDocument();
+      });
     });
   });
 });
-
-/* TODO 로그인 로직 자체는 e2e테스트로 작성을 해야 한다.
-[ ]context
-  로그인 이미지를 누면 
-it
-  로그인 url로 넘어간다 or 갔다가 뭘 받아온다
-[ ] context
-  로그인 과정에서 에러가 발생한다면
-it
-  에러 메시지를 출력해준다.
-*/
