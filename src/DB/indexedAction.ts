29819294a3ab00b3f6660e89f166914569a5c7d5
FerrariRoma <win9592@gmail.com>
2025-04-23 14:17:56 +0900

    Update : #211 AddTodoSchema 추출

--- src/DB/indexedAction.ts
+++ src/DB/indexedAction.ts
@@ -46,6 +46,13 @@ export const TodoSchema = z.object({
   id: z.string().uuid(),
 });
 
+export const AddTodoSchema = TodoSchema.pick({
+  duration: true,
+  todo: true,
+  date: true,
+  categories: true,
+});
+
 type TodoEntity = z.infer<typeof TodoSchema>;
 
 type TransactionMode = 'readonly' | 'readwrite';
