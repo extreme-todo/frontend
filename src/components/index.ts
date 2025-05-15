68ff721f913090c213f551bb84ebd025c7cda86d
imhjnoh <imhjnoh@gmail.com>
2025-05-15 22:04:54 +0900

    Hotfix : #195 미사용 import 제거

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Welcome } from './Welcome';
 export { default as FocusedTime } from './FocusedTime';
-export { default as Setting } from './Setting';
 export { default as Modal } from './Modal';
 export { default as TodoList } from './TodoList';
 export { default as MainTodo } from './MainTodo/MainTodo';
