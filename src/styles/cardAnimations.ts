5305e98f78e2993aceb992e2571ead7cb2b0d05d
imhjnoh <imhjnoh@gmail.com>
2025-07-13 15:11:27 +0900

    Update : #240 카드 애니메이션 맨 위 카드가 사라질 때 opacity 가 0 이 되도록 수정

--- src/styles/cardAnimations.ts
+++ src/styles/cardAnimations.ts
@@ -15,7 +15,7 @@ export const CardAnimationStyle = css`
       left: -28.5px;
       top: -146.52px;
       z-index: 0;
-      opacity: 1;
+      opacity: 0;
       pointer-events: none;
     }
     100% {
