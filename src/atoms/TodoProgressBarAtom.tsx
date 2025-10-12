185fcc2ff23249753143cd6269777b8992562bda
imhjnoh <imhjnoh@gmail.com>
2025-10-12 15:23:28 +0900

    Update : #240 코드 리뷰 반영 - TodoProgressBarAtom Props 필터링

--- src/atoms/TodoProgressBarAtom.tsx
+++ src/atoms/TodoProgressBarAtom.tsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
 import { useIsMobile } from '../hooks';
 
-const TodoProgressBarContainer = styled.div<{
+const TodoProgressBarContainer = styled('div', {
+  shouldForwardProp: (prop) => !['progress', 'type'].includes(prop as string),
+})<{
   progress: number;
   type: 'primary1' | 'primary2' | 'extreme1' | 'extreme2';
 }>`
@@ -458,52 +460,3 @@ export const TodoProgressBarAtom = (props: {
     </TodoProgressBarContainer>
   );
 };
-
-// export const TodoProgressBarAtom = styled.div<{
-//   progress: number;
-//   type: 'primary1' | 'primary2' | 'extreme1' | 'extreme2';
-// }>`
-//   background-color: ${({ theme, type }) => {
-//     switch (type) {
-//       case 'primary1':
-//         return theme.color.backgroundColor.primary1;
-//       case 'primary2':
-//         return theme.color.backgroundColor.primary2;
-//       case 'extreme1':
-//         return theme.color.backgroundColor.extreme_dark;
-//       case 'extreme2':
-//         return theme.color.backgroundColor.extreme_orange;
-//     }
-//   }};
-//   height: 0.75rem;
-//   width: 100%;
-//   border-radius: 1.75rem;
-//   display: flex;
-//   align-items: center;
-//   position: relative;
-//   box-sizing: border-box;
-//   overflow: hidden;
-//   .progress {
-//     width: ${({ progress }) => `${progress}%`};
-//     height: 100%;
-//     line-height: 2.875rem;
-//     border-radius: 3.125rem;
-//     background: ${({ theme }) => theme.color.backgroundColor.extreme_orange};
-//     transition: all 0.2s ease-in-out;
-//   }
-//   @media ${({ theme }) => theme.responsiveDevice.tablet_v},
-//     ${({ theme }) => theme.responsiveDevice.mobile} {
-//     width: 100%;
-//     height: 100%;
-//     align-items: flex-start;
-//     justify-content: center;
-//     padding: 1rem 0 1rem 0;
-//     .progress {
-//       height: ${({ progress }) => `${progress}%`};
-//       width: 70%;
-//       overflow: hidden;
-//       padding: 0;
-//       color: transparent;
-//     }
-//   }
-// `;
