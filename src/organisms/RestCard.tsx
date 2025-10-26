b0e7ca6313343710ebc248a14f5a2d991f2b1245
imhjnoh <imhjnoh@gmail.com>
2025-10-26 15:24:47 +0900

    Update : #244 휴식 화면 모바일 디자인 적용

--- src/organisms/RestCard.tsx
+++ src/organisms/RestCard.tsx
@@ -52,93 +52,91 @@ export function RestCard({
     [isExtreme, canRest, doTodo, actions],
   );
   return (
-    <CardAtom
-      className="card"
-      bg={'primary2'}
-      padding={isMobile ? '0' : undefined}
-    >
-      {pomodoro.status === PomodoroStatus.RESTING && (
-        <RestCardWrapper>
-          {isMobile && (
-            <div className="mobile-header-wrapper">
-              <div className="mobile-top-button-slot">
-                {mobileTopButtonSlot}
-              </div>
-              <ExtremeModeIndicator />
-            </div>
-          )}
-          {!isMobile && (
-            <div className="desktop-extreme-wrapper">
-              <ExtremeModeIndicator />
-            </div>
-          )}
-          <div className="center-container">
-            <TypoAtom
-              fontSize={'h3'}
-              fontColor={isExtreme ? 'extreme_dark' : 'primary1'}
-              className="left-time"
-            >
-              남은 휴식시간
-            </TypoAtom>
-            <Clock
-              ms={getLeftMs()}
-              show={{
-                hour: false,
-                min: true,
-                sec: true,
-              }}
-              fontColor={isExtreme ? 'extreme_dark' : 'primary1'}
-            ></Clock>
-            {!isMobile && getButtonContainer}
+    <RestCardContainer>
+      <CardAtom
+        className="card"
+        bg={'primary2'}
+        padding={isMobile ? '0' : undefined}
+      >
+        {isMobile && (
+          <div className="mobile-header-wrapper">
+            <div className="mobile-top-button-slot">{mobileTopButtonSlot}</div>
+            <ExtremeModeIndicator />
           </div>
+        )}
+        {!isMobile && (
+          <div className="desktop-extreme-wrapper">
+            <ExtremeModeIndicator />
+          </div>
+        )}
+        {pomodoro.status === PomodoroStatus.RESTING && (
+          <RestCardWrapper>
+            <div className="center-container">
+              <TypoAtom
+                fontSize={'h3'}
+                fontColor={isExtreme ? 'extreme_dark' : 'primary1'}
+                className="left-time"
+              >
+                남은 휴식시간
+              </TypoAtom>
+              <Clock
+                ms={getLeftMs()}
+                show={{
+                  hour: false,
+                  min: true,
+                  sec: true,
+                }}
+                fontColor={isExtreme ? 'extreme_dark' : 'primary1'}
+              ></Clock>
+              {!isMobile && getButtonContainer}
+            </div>
 
-          <div className="progress-container">
-            <TodoProgressBarAtom
-              type={isExtreme ? 'extreme1' : 'primary1'}
-              progress={Math.floor(
-                ((pomodoro.time ?? 0) / (pomodoro.settings.restStep * 60000)) *
-                  100,
-              )}
-            ></TodoProgressBarAtom>
-          </div>
-
-          <div className="todo-title">
-            <div className="todo-duration">
-              <TypoAtom fontSize={'h3'}>{currentRound + ' Round'}</TypoAtom>
-              <div>
-                <TypoAtom fontSize="h3">{`🍅 `.repeat(currentRound)}</TypoAtom>
-                <TypoAtom fontSize="h3" className="left-round">
-                  {`🍅 `.repeat(
-                    Math.max(todo?.duration ?? 0 - currentRound, 0),
-                  )}
-                </TypoAtom>
-              </div>
+            <div className="progress-container">
+              <TodoProgressBarAtom
+                type={isExtreme ? 'extreme1' : 'primary1'}
+                progress={Math.floor(
+                  ((pomodoro.time ?? 0) /
+                    (pomodoro.settings.restStep * 60000)) *
+                    100,
+                )}
+              ></TodoProgressBarAtom>
             </div>
-            <TypoAtom fontSize={'h2'} fontColor="primary1">
-              {todo?.todo}
-            </TypoAtom>
-            {isMobile && getButtonContainer}
-          </div>
-        </RestCardWrapper>
-      )}
-    </CardAtom>
+
+            <div className="todo-title">
+              <div className="todo-duration">
+                <TypoAtom fontSize={'h3'}>{currentRound + ' Round'}</TypoAtom>
+                <div>
+                  <TypoAtom fontSize="h3">
+                    {`🍅 `.repeat(currentRound)}
+                  </TypoAtom>
+                  <TypoAtom fontSize="h3" className="left-round">
+                    {`🍅 `.repeat(
+                      Math.max(todo?.duration ?? 0 - currentRound, 0),
+                    )}
+                  </TypoAtom>
+                </div>
+              </div>
+              <TypoAtom fontSize={'h2'} fontColor="primary1">
+                {todo?.todo}
+              </TypoAtom>
+              {isMobile && getButtonContainer}
+            </div>
+          </RestCardWrapper>
+        )}
+      </CardAtom>
+    </RestCardContainer>
   );
 }
 
-const RestCardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
-  width: 90%;
-  max-width: 90%;
-  height: 100%;
-  position: relative;
-  gap: 2.56rem;
+const RestCardContainer = styled.div`
   .mobile-header-wrapper {
     width: 100%;
+    padding: 1.25rem;
+    height: 1.75rem;
     display: flex;
     justify-content: space-between;
+    box-sizing: border-box;
+    align-items: flex-start;
   }
   .desktop-extreme-wrapper {
     width: 100%;
@@ -146,6 +144,18 @@ const RestCardWrapper = styled.div`
     justify-content: flex-end;
     box-sizing: border-box;
   }
+`;
+
+const RestCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  gap: 2.56rem;
+
   .center-container {
     display: flex;
     width: 100%;
