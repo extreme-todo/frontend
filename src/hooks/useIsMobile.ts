3c208db0c37b87c4e7531e3707c1c7327102ea98
imhjnoh <imhjnoh@gmail.com>
2025-10-26 15:24:19 +0900

    Fix : #244 useIsMobile 오류 수정 - mobile -> desktop 이벤트가 트리거 되지 않는 이슈 조치

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,22 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { responsiveBreakpoints } from '../shared/constants';
 
 export const useIsMobile = () => {
+  const responsiveMobileRef = useRef<boolean>(false);
   const [responsiveMobile, setResponsiveMobile] = useState<boolean>(false);
+
+  const updateResponsiveMobile = (value: boolean) => {
+    responsiveMobileRef.current = value;
+    setResponsiveMobile(responsiveMobileRef.current);
+  };
+
   useEffect(() => {
     if (window.ResizeObserver)
       new ResizeObserver((entries) => {
         const { borderBoxSize } = entries[0];
-        if (
-          borderBoxSize[0].inlineSize > responsiveBreakpoints.mobile.min &&
-          borderBoxSize[0].inlineSize < responsiveBreakpoints.tablet_v.max
-        ) {
-          if (responsiveMobile !== true) {
-            setResponsiveMobile(true);
+        if (borderBoxSize[0].inlineSize < responsiveBreakpoints.tablet_v.max) {
+          if (responsiveMobileRef.current !== true) {
+            updateResponsiveMobile(true);
           }
         } else {
-          if (responsiveMobile !== false) {
-            setResponsiveMobile(false);
+          console.log(responsiveMobileRef.current, 'is desktop');
+          if (responsiveMobileRef.current !== false) {
+            updateResponsiveMobile(false);
           }
         }
       }).observe(document.body);
